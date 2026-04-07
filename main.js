0451388d226486f89cba8000a818657087d13ea3
s0m1k111 <spavel1207@gmail.com>
2026-04-07 14:58:51 +0300

    Step 6: functions in JavaScript

--- main.js
+++ main.js
@@ -42,4 +42,55 @@ for (let i = 1; i <= 4; i++) {
     }
     console.log(a);
 }
-
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(3,5));
+function multiply(a, b) {
+    return a*b;
+}
+console.log(multipl(34,67));
+function sayHello(name){
+    console.log(`Hello, ${name}`);
+}
+sayHello("Аринка-мандаринка")
+function printInfo(name , age) {
+    console.log(`Name: ${name} Age: ${age}`);
+}
+printInfo("акакий", 42);
+function greet(name = "Гость"){
+    console.log("Привет, " + name);
+}
+greet();
+greet("Сашка какашка");
+function calculateDiscount(c , p = 10) {
+    return c - p;
+}
+calculateDiscount(34);
+const add = function (a, b){
+    return a + b;
+};
+console.log(add(2, 3));
+function makeCounter(){
+    let count = 0;
+    return function () {
+        count++;
+        return count;
+    };
+}
+const counter = makeCounter();
+console.log(counter());
+console.log(counter());
+console.log(counter());
+function sumFuns(a, b) {
+    return a + b;
+}
+const sumFuns2 = (a,b) => a + b;
+const double = x => x * 2;
+const sumFunc3 = (a, b) => a + b;
+const sayGreeting = () => console.log("htllo");
+const square = x => x * x;
+const calculate = (a, b) => {
+    let result = a + b;
+    return result * 2;
+}
